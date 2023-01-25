c60680041a5c811e1a58fe63dc67c3e0e19aeb8c
alaxhenry0121 <alaxhenry0121@gmail.com>
2023-01-25 21:51:11 +0900

    Initial Commit

---
+++ DropDown/DropDownApp.swift
@@ -0,0 +1,17 @@
+//
+//  DropDownApp.swift
+//  DropDown
+//
+//  Created by Seungchul Ha on 2023/01/25.
+//
+
+import SwiftUI
+
+@main
+struct DropDownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
